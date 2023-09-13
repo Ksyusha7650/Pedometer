c22769b787fa16adc18e9086e6a9e295142a4989
Ksu <ksysha.2002r@gmail.com>
2023-09-14 02:10:34 +0300

    start making 2 lab

---
+++ app/src/main/java/com/example/pedometer/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.pedometer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToSettings(View view){
+        Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
+        MainActivity.this.startActivity(myIntent);
+    }
+}
